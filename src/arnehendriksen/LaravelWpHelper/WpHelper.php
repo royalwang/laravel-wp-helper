264a982ffa254b9d1955a784465218750876920b
Arne Hendriksen <arne.hendriksen@gmail.com>
2016-08-23 21:56:42 +0200

    Added the function option($option_name). This returns the option_value from wp_options, based on the given option_name.

--- src/arnehendriksen/LaravelWpHelper/WpHelper.php
+++ src/arnehendriksen/LaravelWpHelper/WpHelper.php
@@ -8,4 +8,17 @@ class WpHelper
 
     }
 
+    /**
+     * Return the option_value from wp_options, based on the given option_name.
+     *
+     * @param $option_name
+     * @return mixed
+     */
+    public function option($option_name) {
+        $option = DB::table('wp_options')->select('option_value')->where('option_name','=',$option_name)->first();
+        if ( $option ) {
+            return $option->option_value;
+        }
+    }
+
 }
